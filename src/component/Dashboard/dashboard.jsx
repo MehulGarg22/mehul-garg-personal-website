ff36d7406d874e521a3090deeaedf1e33de4efba
MEHUL GARG <mehul.garg104@gmail.com>
2024-01-17 16:55:11 +0530

    Changes

--- src/component/Dashboard/dashboard.jsx
+++ src/component/Dashboard/dashboard.jsx
@@ -4,15 +4,15 @@ import Button from 'react-bootstrap/Button';
 import Home from "../Home/home";
 import About from '../About/about'
 import Skills from "../Skills/skills";
-
+import './dashboard.css'
 export default function Dashboard(){
 
     return(
-        <>
+        <div className="dashboardContainer">
             <NavbarMain/>
             <Home />
             <About />
             <Skills />
-        </>
+        </div>
     );
 }
--- src/component/Skills/skills.jsx
+++ src/component/Skills/skills.jsx
@@ -1,10 +1,70 @@
 import React from "react";
 import Heading from "../Heading/heading";
-
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './skills.css';
 export default function Skills(){
     return(
-        <div>
+        <div className="skillContainer">
             <Heading heading={"__Skills__"} />
+            <Row xs={1} md={2} className="g-4">
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <br/>
+            <Row xs={1} md={2} className="g-4">
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 }
