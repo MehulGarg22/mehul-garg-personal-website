19fb3b2ac06f16386084876af5fcb141218c5c29
MEHUL GARG <mehul.garg104@gmail.com>
2024-01-17 17:49:49 +0530

    constantsjs

--- src/component/Skills/skills.jsx
+++ src/component/Skills/skills.jsx
@@ -4,6 +4,12 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import './skills.css';
+import c from '../../assests/C.png';
+import cplus from '../../assests/C++.png';
+import python from '../../assests/Python.png';
+import sql from '../../assests/SQL.png';
+import js from '../../assests/JS.png';
+
 export default function Skills(){
     return(
         <div className="skillContainer">
@@ -11,22 +17,26 @@ export default function Skills(){
             <Row xs={1} md={2} className="g-4">
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
+                        
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title className="titleText"> Programming Languages</Card.Title>
                         <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit
-                            longer.
+                            <img className="images" src={c} alt="C Programming"/>    
+                            <img className="images"  src={cplus} alt="C++"/>
+                            <img className="images"  src={python} alt="HTML5"/>
+                            <img className="images"  src="../../assests/CSS3.png" alt="CSS3"/>
+                            <img className="images"  src="../../assests/JS.png" alt="JS"/>
+                            <img className="images"  src="../../assests/Python.png" alt="Python"/>
+                            <img className="images"  src="../../assests/SQL.png" alt="SQL"/>
                         </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
+                        
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title className="titleText"> Frameworks</Card.Title>
                         <Card.Text>
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
@@ -40,9 +50,9 @@ export default function Skills(){
             <Row xs={1} md={2} className="g-4">
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
+                        
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title className="titleText"> Databases</Card.Title>
                         <Card.Text>
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
@@ -53,9 +63,25 @@ export default function Skills(){
                 </Col>
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
+                        
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title className="titleText"> Developer Tools</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <br/>
+            <Row xs={1} md={2} className="g-4">
+                <Col>
+                    <Card>
+                        
+                        <Card.Body>
+                        <Card.Title className="titleText"> AWS Services</Card.Title>
                         <Card.Text>
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
--- src/component/constant.js/constant.jsx
+++ src/component/constant.js/constant.jsx
@@ -10,6 +10,36 @@ import twitter from '../../assests/twitter.png';
 import github from '../../assests/github.png';
 import mail from '../../assests/gmail.png';
 
+import c from '../../assests/C.png'
+import cplus from '../../assests/C++.png'
+import html from '../../assests/html5.png'
+import css from '../../assests/CSS3.png'
+import js from '../../assests/JS.png'
+import python from '../../assests/Python.png'
+import sql from '../../assests/SQL.png';
+import react from '../../assests/React.png';
+import node from '../../assests/Nodejs.png';
+import boostrap from '../../assests/bootstrap.png';
+import redux from '../../assests/Redux.png';
+import yarn from '../../assests/yarn.png'
+import mysql from '../../assests/MySQL.png'
+import postgresql from '../../assests/PostgreSQL.png'
+import mongodb from '../../assests/MongoDb.png'
+import npm from '../../assests/npm.png'
+import postman from '../../assests/postman-icon.png';
+import git from '../../assests/git.png';
+import githubs from '../../assests/github.png';
+import vscode from '../../assests/VScode.png';
+import lambda from '../../assests/Lambda.png';
+import dynamodb from '../../assests/DynamoDB.png'
+import apigateway from '../../assests/API Gateway.png'
+import iam from '../../assests/IAM Identity Center.png'
+import amplify from '../../assests/Amplify.png'
+import codecommit from '../../assests/CodeCommit.png';
+import cognito from '../../assests/Cognito.png';
+import s3 from '../../assests/Simple Storage Service.png';
+
+
 export  let array=[
     {
         text: "About",
@@ -58,4 +88,145 @@ export let homeArray=[
         imageSource: mail,
         socialLink: "mailto: mehul.garg104@gmail.com",
     },
+]
+
+
+export let skillsArray=[
+    {
+        heading:"Programming Language",
+        programmingLanguage:[
+            {
+                imageAlt:"C",
+                image: c
+            },
+            {
+                imageAlt:"Cplus",
+                image: cplus
+            },
+            {
+                imageAlt:"html",
+                image: html
+            },
+            {
+                imageAlt:"css",
+                image: css
+            },
+            {
+                imageAlt:"js",
+                image: js
+            },
+            {
+                imageAlt:"python",
+                image: python
+            },
+            {
+                imageAlt:"sql",
+                image: sql
+            }
+        ]
+    },
+    {
+        heading:"Framework",
+        programmingLanguage:[
+            {
+                imageAlt:"react",
+                image: react
+            },
+            {
+                imageAlt:"redux",
+                image: redux
+            },
+            {
+                imageAlt:"boostrap",
+                image: boostrap
+            },
+            {
+                imageAlt:"yarn",
+                image: yarn
+            },
+            {
+                imageAlt:"node",
+                image: node
+            }
+        ]
+    },
+    {
+        heading:"Database",
+        programmingLanguage:[
+            {
+                imageAlt:"MySQL",
+                image: mysql
+            },
+            {
+                imageAlt:"postgresql",
+                image: postgresql
+            },
+            {
+                imageAlt:"mongodb",
+                image: mongodb
+            }
+        ]
+    },
+    {
+        heading:"Developer Tools",
+        programmingLanguage:[
+            {
+                imageAlt:"npm",
+                image: npm
+            },
+            {
+                imageAlt:"postman",
+                image: postman
+            },
+            {
+                imageAlt:"git",
+                image: git
+            },
+            {
+                imageAlt:"github",
+                image: github
+            },
+            {
+                imageAlt:"vscode",
+                image: vscode
+            }
+        ]
+    },
+    {
+        heading:"AWS Services",
+        programmingLanguage:[
+            {
+                imageAlt:"Lambda",
+                image: lambda
+            },
+            {
+                imageAlt:"Dynamodb",
+                image: dynamodb
+            },
+            {
+                imageAlt:"apigateway",
+                image: apigateway
+            },
+            {
+                imageAlt:"iam",
+                image: iam
+            },
+            {
+                imageAlt:"amplify",
+                image: amplify
+            },
+            {
+                imageAlt:"codecommit",
+                image: codecommit
+            },
+            {
+                imageAlt:"cognito",
+                image: cognito
+            },
+            {
+                imageAlt:"s3",
+                image: s3
+            }
+        ]
+    }
 ]
